bd3d006bfc2830542fc85007fe3543e9a71cde9e
Seagull <ssean5739@gmail.com>
2021-07-23 23:17:47 +0300

    Finished task 1

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const blackSquare = document.querySelector("#black-square");
+let blackSquare = document.querySelector("#black-square");
+const cssBtn = document.querySelector(".task1__btn-css");
 const jsBtn = document.querySelector(".task1__btn-js");
 const cssJsBtn = document.querySelector(".task1__btn-css-js");
+const squareWrapper = document.querySelector(".task1__square-wrapper");
 
-jsBtn.addEventListener("click", function() {
+/**
+ * Deletes / renders back the black square by
+ * accessing DOM style properties
+ */
+cssBtn.addEventListener("click", function() {
   if (blackSquare.style.visibility === "hidden") {
     blackSquare.style.visibility = "visible";
   } else {
@@ -10,6 +16,25 @@ jsBtn.addEventListener("click", function() {
   }
 });
 
+/**
+ * Deletes / renders back the black square by
+ * working with DOM element`s using js
+ */
+jsBtn.addEventListener("click", function() {
+  if (squareWrapper.innerHTML !== "") {
+    squareWrapper.innerHTML = "";
+  } else {
+    blackSquare = document.createElement("div");
+    blackSquare.classList.add("task1__square");
+    blackSquare.id = "black-square";
+    squareWrapper.insertAdjacentElement("afterbegin", blackSquare);
+  }
+});
+
+/**
+ * Deletes / renders back the black square by
+ * working with DOM element`s classes
+ */
 cssJsBtn.addEventListener("click", function() {
   blackSquare.style.visibility = "";
 
