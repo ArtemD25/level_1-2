c71fd568a656aee0da62a68785fbaac5b4295a98
Seagull <ssean5739@gmail.com>
2021-07-24 14:41:34 +0300

    Finished task 4 and finally commited js-file

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+'use strict'
+
+//* Task 1
+
 let blackSquare = document.querySelector("#black-square");
 const cssBtn = document.querySelector(".task1__btn-css");
 const jsBtn = document.querySelector(".task1__btn-js");
@@ -25,7 +29,7 @@ jsBtn.addEventListener("click", function() {
     squareWrapper.innerHTML = "";
   } else {
     blackSquare = document.createElement("div");
-    blackSquare.classList.add("task1__square");
+    blackSquare.classList.add("square");
     blackSquare.id = "black-square";
     squareWrapper.insertAdjacentElement("afterbegin", blackSquare);
   }
@@ -40,3 +44,82 @@ cssJsBtn.addEventListener("click", function() {
 
   blackSquare.classList.toggle("hidden");
 });
+
+//* Task 2
+
+const btnTask2 = document.querySelector(".task2__btn");
+const blackSquare2 = document.querySelector(".task2__wrapper .square");
+
+/**
+ * Deletes / renders back the black square by
+ * using classList.toggle
+ */
+btnTask2.addEventListener("click", function() {
+  blackSquare2.classList.toggle("hidden");
+})
+
+//* Task 3
+
+const btnTask3 = document.querySelector(".task3__btn");
+const blackSquare3 = document.querySelectorAll(".task3__squares .square");
+
+/**
+ * Deletes / renders back 5 black squares
+ * at once using for-of loop
+ */
+btnTask3.addEventListener("click", function() {
+  for (let blackSquare of blackSquare3) {
+    blackSquare.classList.toggle("hidden");
+  }
+});
+
+//* Task 4
+
+const btnTask4 = document.querySelector(".task4__btn");
+const inputTask4 = document.getElementById("task4__input");
+
+btnTask4.addEventListener("click", function() {
+  const inputValue = inputTask4.value;
+  const squareSelector = ".task4__squares " + "." + inputValue;
+
+  if (!inputValue) {
+    inputTask4.value = "";
+    inputTask4.placeholder = `Enter proper value`;
+    return;
+  }
+  
+  if (isThereSuchSelector(squareSelector)) {
+    document.querySelector(squareSelector).classList.toggle("hidden");
+  } else {
+    inputTask4.value = "";
+    inputTask4.placeholder = `Element .${inputValue} does not exist`;
+  }
+});
+
+function isThereSuchSelector(squareSelector) {
+  if (document.querySelector(squareSelector) !== null) {
+    return true;
+  }
+  return false;
+}
+
+//* Task 5
+
+//* Task 6
+
+//* Task 7
+
+//* Task 8
+
+//* Task 9
+
+//* Task 10
+
+//* Task 11
+
+//* Task 12
+
+//* Task 13
+
+//* Task 14
+
